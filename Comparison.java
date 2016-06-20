e1079c482b7f000b126cb417ac89d1bc3baf3c0e
vprincelouie <vprincelouie@gmail.com>
2016-06-20 11:33:54 +0800

    Update Comparison.java
    YES SIR! CAVS <3 1-3 NAG DAOG PA HAHA

--- Comparison.java
+++ Comparison.java
@@ -1,32 +1,32 @@
-import java.util.Scanner; // program uses class Scanner  
-public class Comparison  
-{    
-    // main method begins execution of Java application    
-    public static void main( String args[] )    
+import java.util.Scanner; // program uses class Scanner
+public class Comparison
+{
+    // main method begins execution of Java application
+    public static void main( String []args )
     {
-        // create Scanner to obtain input from command window       
-        Scanner input = Scanner( System.in );  
-        int number1; // first number to compare       
-        int number2; // second number to compare  
-      
-        System.out.prnt( "Enter first integer: " ); // prompt        
-        number1 = input.nextInt(); // read first number from user   
- 
-        System.out.print( "Enter second integer: " ); // prompt        
-        num2 = input.nextInt(); // read second number from user               
-        
-        if ( number1 == number2 )           
-            Systm.out.printf( "%d == %d\n", number1, number2 );  
-        if ( number1 != number2 )          
-            System.out.printf( "%d != %d\n", number1, number2 );  
-        if ( number1 < number2 )          
-            System.out.printf( "%d < %d\n", number1, number2 );  
-        if ( number1 > number2 )          
-            System.out.printf( "%d > %d\n", number1, number2 );  
-        if ( number1 <= number2 )          
-            System.out.printf( "%d <= %d\n", number1, number2 );  
-        if ( number1 >= number2 )          
-            System.out.printf( "%d >= %d\n", number1, number2 );  
-    }
-   } // end method main  
-} // end class Comparison 
+        // create Scanner to obtain input from command window
+        Scanner input = new Scanner( System.in );
+        int number1; // first number to compare
+        int number2; // second number to compare
+
+        System.out.print( "Enter first integer: " ); // prompt
+        number1 = input.nextInt(); // read first number from user
+
+        System.out.print( "Enter second integer: " ); // prompt
+        number2 = input.nextInt(); // read second number from user
+
+        if ( number1 == number2 )
+            System.out.printf( "%d == %d\n", number1, number2 );
+        if ( number1 != number2 )
+            System.out.printf( "%d != %d\n", number1, number2 );
+        if ( number1 < number2 )
+            System.out.printf( "%d < %d\n", number1, number2 );
+        if ( number1 > number2 )
+            System.out.printf( "%d > %d\n", number1, number2 );
+        if ( number1 <= number2 )
+            System.out.printf( "%d <= %d\n", number1, number2 );
+        if ( number1 >= number2 )
+            System.out.printf( "%d >= %d\n", number1, number2 );
+
+   } // end method main
+} // end class Comparison
